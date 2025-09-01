7ce70332b5ec2958b64a74d194cd79eb11e901a2
Archie1711 <karloarnold@gmail.com>
2025-09-02 01:38:49 +0200

    Update Team.h

--- Team.h
+++ Team.h
@@ -1,16 +1,31 @@
 #ifndef TEAM_H
 #define TEAM_H
-#include "player.h"
+
+/* Forward declaration – izbjegne kružne include-ove */
+typedef struct Player Player;
 
 #define MAX_FANTASY_PLAYERS 9
 
 typedef struct {
     Player* selectedPlayers[MAX_FANTASY_PLAYERS];
-    int playerCount;
+    int     playerCount;
 } FantasyTeam;
 
+/* Upravljanje timom (meni) */
 void manageTeams(Player* allPlayers, int playerCount);
 
-#endif // TEAM_H
+/* Simulacija aktivnog gameweeka (podiže GW +1 kad završi) */
+void simulate_current_gameweek(Player* allPlayers, int playerCount);
 
+/* Gameweek status */
+int  team_get_current_gameweek(void);
+void team_advance_gameweek(void);
+int  team_is_season_over(void);
 
+/* Iz team.c – javna konstanta */
+extern const int MAX_GAMEWEEKS;
+
+/* Read-only pristup trenutnom timu */
+const FantasyTeam* team_get_user_team(void);
+
+#endif /* TEAM_H */
