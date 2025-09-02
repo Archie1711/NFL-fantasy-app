9b8f6e8c3b2220ccacb4179d59f7cbd5d40efc31
Archie1711 <karloarnold@gmail.com>
2025-09-02 07:19:52 +0200

    Update Player.h

--- Player.h
+++ Player.h
@@ -8,7 +8,7 @@
 /* Tagirani struct da forward-decl u team.h radi */
 typedef struct Player {
     int  id;
-    char name[MAX_NAME_LENGTH];     /* npr. Kyler_Murray */
+    char name[MAX_NAME_LENGTH];     /* pr. Kyler_Murray */
     char position[MAX_POSITION];    /* "QB","RB","WR","TE","K" */
     int  rating;                    /* npr. 80 */
     int  price;                     /* npr. 14 */
@@ -19,10 +19,6 @@ typedef struct Player {
 Player* loadPlayersFromFile(const char* filename, int* playerCount);
 void    showAllPlayers(Player* players, int count);
 
-/* CRUD nad players.txt (radi na memorijskom nizu + piše u datoteku)
-   Sve funkcije ažuriraju *players i *count i prepisuju cijeli players.txt.
-   Vraćaju 0=OK, -1=err.
-*/
 int insert_player(const char* filename, Player** players, int* count, const Player* p);
 int update_player(const char* filename, Player** players, int* count, const Player* p);
 int delete_player(const char* filename, Player** players, int* count, int id);
@@ -36,3 +32,4 @@ void sort_players_by_name(Player* arr, int n);
 Player* find_player_by_id(Player* arr, int n, int id);
 
 #endif /* PLAYER_H */
+
