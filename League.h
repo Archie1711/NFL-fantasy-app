1eff3865a936e3eb54bef846b3563dc6bc1b382f
Archie1711 <karloarnold@gmail.com>
2025-09-02 07:13:56 +0200

    Update League.h

--- League.h
+++ League.h
@@ -1,12 +1,6 @@
 #ifndef LEAGUE_H
 #define LEAGUE_H
 
-/* Jednostavna “liga” za jednog managera ("You"):
-   - bilježimo bodove po gameweeku (1..17)
-   - računamo ukupni zbroj
-   - možemo ispisati tablicu i zapisati je u league_table.txt
-*/
-
 void league_init(void);
 void league_record_result(const char* manager_name, int gw, double points);
 void league_show_table(void);
